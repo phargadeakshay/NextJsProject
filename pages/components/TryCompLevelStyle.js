c3b1ee6a26a86c83f5133aa564f9c4c73caae658
phargadeakshay <phargadeakshay@gmail.com>
2022-10-12 11:49:26 +0530

    Internal CSS in components

---
+++ pages/components/TryCompLevelStyle.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TryCompLevelStyle = () => {
+  return (<>
+    <style jsx>
+    {`
+    .container{
+    color:red;
+    }
+    `}</style>
+
+    <div className='container'>TryCompLevelStyle</div>
+    </>
+  )
+}
+
+export default TryCompLevelStyle
---
+++ pages/components/home.js
@@ -0,0 +1,16 @@
+import TryCompLevelStyle from "./TryCompLevelStyle";
+
+import React from 'react'
+
+const home = () => {
+  return (
+    <div>
+        <TryCompLevelStyle/>
+        <div>
+<p>home</p>
+        </div>
+    </div>
+  )
+}
+
+export default home
