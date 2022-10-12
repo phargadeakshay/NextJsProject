406db1ea356352373ef885fdcd7c5e80938313cf
Akshay Phargade <phargadeakshay@gmail.com>
2022-10-12 15:15:28 +0530

    node readFile and pass json obj to frontend

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import * as fs from "fs";
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  fs.readFile("AksJson/aks.json","utf-8",(error,data)=>{
+
+    console.log(typeof data)
+    const jsondata=JSON.parse(data)
+    console.log(typeof jsondata)
+    console.log(jsondata)
+    res.status(200).json(jsondata)
+    
+  })
 }
